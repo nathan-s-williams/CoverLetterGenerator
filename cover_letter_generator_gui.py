d2839378cf44d06e0f506285a4a5d0e69ec6710f
Nathan Williams <nate88williams@gmail.com>
2023-09-03 20:49:34 -0500

    Completed basic insertion fields and functionality to create document.

--- cover_letter_generator_gui.py
+++ cover_letter_generator_gui.py
@@ -1,16 +1,25 @@
 import PySimpleGUI as sg
+import subprocess as sp
+from cover_letter import CoverLetter
 
 sg.theme("dark blue 3")
 
 input_column = [
-    [sg.Text("Cover Letter Type")],
-    [sg.Radio("Web", "RADIO-TYPE", default=True),
-     sg.Radio("New", "RADIO-TYPE")],
-    [sg.Text("File Format")],
-    [sg.Radio("Word", "RADIO-FORMAT", default=True),
-     sg.Radio("Text", "RADIO-FORMAT")],
+    [
+        sg.Text("Cover Letter Type", size=(15, 1)),
+        sg.Radio("Web", "RADIO-TYPE", default=True, key="-WEB-"),
+        sg.Radio("New", "RADIO-TYPE", key="-NEW-")
+    ],
+    [
+        sg.Text("File Format", size=(15, 1)),
+        sg.Radio("Word", "RADIO-FORMAT", default=True, key="-WORD-"),
+        sg.Radio("Text", "RADIO-FORMAT", key="-TEXT-")
+    ],
     [sg.Text("Enter Date")],
-    [sg.InputText(key="-DATE-", size=(35, 1)), sg.CalendarButton("Calendar", target="-DATE-")],
+    [
+        sg.InputText(key="-DATE-", size=(34, 1)),
+        sg.CalendarButton("Calendar", target="-DATE-")
+    ],
     [sg.Text("Enter Company")],
     [sg.InputText(key="-COMPANY-")],
     [sg.Text("Enter Job Role Name")],
@@ -24,7 +33,8 @@ layout = [
         sg.Column(input_column),
         sg.VSeparator(),
         sg.Column(view_column)
-    ]
+    ],
+    [sg.Button("Create"), sg.Button("Cancel")]
 
 ]
 
@@ -32,7 +42,30 @@ window = sg.Window(title="Cover Letter Generator", layout=layout, margins=(200, 
 
 while True:
     event, values = window.read()
-    if event == sg.WINDOW_CLOSED:
+    if event in (sg.WINDOW_CLOSED, "Cancel"):
         break
+    elif event == "Create":
+        if values["-WEB-"]:
+            new_cover_letter = CoverLetter("web")
+        else:
+            new_cover_letter = CoverLetter("new")
+
+        try:
+            new_cover_letter.set_date(values["-DATE-"])
+            new_cover_letter.set_job_role(values["-JOBNAME-"])
+            new_cover_letter.set_company(values["-COMPANY-"])
+            new_cover_letter.insert_data()
+
+            if values["-WORD-"]:
+                new_cover_letter.generate_word_doc()
+            else:
+                new_cover_letter.generate_text_doc()
+
+            new_cover_letter.clean_up()
+            sp.call(["open", "./cover_letter_outputs"])
+
+        except Exception as e:
+            print(e)
+            sg.popup_error(f"An error occurred:\n {str(e)}")
 
 window.close()
